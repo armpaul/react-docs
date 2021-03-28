857334dffcc14f9619b72e58c859592db41d0548
Nisharg Shah <nishargshah3101@gmail.com>
2021-03-28 21:10:25 +0530

    changed urls of image mapper

--- src/stories/codes/common.js
+++ src/stories/codes/common.js
@@ -5,11 +5,11 @@ const common = code =>
 import ImageMapper from 'react-img-mapper';
 
 const Mapper = props => {
-  const URL = 'https://raw.githubusercontent.com/NishargShah/react-img-mapper/master/src/assets/example.jpg';
+  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
   const MAP = {
     name: 'my-map',
     // GET JSON FROM BELOW URL AS AN EXAMPLE
-    areas: 'https://raw.githubusercontent.com/NishargShah/react-img-mapper/master/src/assets/example.json',
+    areas: 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.json',
   };
   
   return ${code}
